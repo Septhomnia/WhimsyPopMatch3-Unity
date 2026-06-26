e000361315233c4cc29a6acd611ffff9cff4f75e
Esra Rüzgar <e.1042.q@gmail.com>
2026-06-26 21:31:55 +0300

    fixed swipe controls

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -23,13 +23,21 @@ public class BoardManager : MonoBehaviour
     {
         for (int i = 0; i < width; i++)
         {
-            for(int j = 0; j < height; j++)
+            for (int j = 0; j < height; j++)
             {
                 Vector2 tempPosition = new Vector2(i, j);
-                GameObject Tile = Instantiate(tilePrefab, tempPosition ,Quaternion.identity) as GameObject;
+                GameObject Tile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
                 Tile.transform.parent = this.transform;
                 Tile.name = "(" + i + "," + j + ")";
                 int dotToUse = Random.Range(0, dots.Length);
+                int maxIterations = 0;
+                while (MatchesAt(i, j, dots[dotToUse]) && maxIterations < 100)
+                {
+                    dotToUse = Random.Range(0, dots.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
                 GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
                 dot.transform.parent = this.transform;
                 dot.name = "(" + i + "," + j + ")";
@@ -37,5 +45,36 @@ public class BoardManager : MonoBehaviour
             }
         }
 
+    }
+    private bool MatchesAt(int column, int row, GameObject piece)
+    {
+        // Horizontal check
+        // Soldaki iki dot ayný tag'e sahip mi?
+        if (column > 1)
+        {
+            if (allDots[column - 1, row] != null && allDots[column - 2, row] != null)
+            {
+                if (allDots[column - 1, row].tag == piece.tag &&
+                    allDots[column - 2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Vertical check
+        if (row > 1)
+        {
+            if (allDots[column, row - 1] != null && allDots[column, row - 2] != null)
+            {
+                if (allDots[column, row - 1].tag == piece.tag &&
+                    allDots[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
